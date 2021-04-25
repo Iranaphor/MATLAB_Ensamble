97e84fbfa6931be481017d195b6cee0b3ad6eb50
James Heselden <34611419+Iranaphor@users.noreply.github.com>
2021-04-25 14:53:56 +0100

    Update dimensions.m

--- dimensions.m
+++ dimensions.m
@@ -1,14 +1,14 @@
 X = 1:7;
 
-d1 = X;
-d2 = X;
-d3 = X;
-d4 = X*20;
-d5 = (X*35)/255; 
-d6 = X/7;
-d7 = (255-(X*25))/255;
-d8 = ['*','o','h','p','s','^','+'];
-d9 = X;
+d1 = X;                             %x-coord
+d2 = X;                             %y-coord
+d3 = X;                             %z-coord
+d4 = X*20;                          %Size
+d5 = (X*35)/255;                    %Red
+d6 = X/7;                           %Green
+d7 = (255-(X*25))/255;              %Blue
+d8 = ['*','o','h','p','s','^','+']; %Shape
+d9 = X;                             %LineWidth
 d10 = "time?";
 d11 = "pacing?";
 % [1 2    3   4 5 6  7           8 9   10] val == space between val & val+1
